7e9a3cfb467f5bbe2711b4f578c595ddf733528e
Adam Hettler <ahettler@outlook.cz>
2024-10-03 17:49:27 +0200

    #1 první program - uživatelský vstup

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("**************************************");
+        Console.WriteLine("******* Výpis číselné řady ***********");
+        Console.WriteLine("********** Adam hettler **************");
+        Console.WriteLine("**************************************");
+        Console.WriteLine();
+        Console.WriteLine();
+        // Vstup od uživatele
+
+        // Console.Write("Zadejte první číslo: ");
+        // int first = int.Parse(Console.ReadLine());
+
+        // Vstup od uživatele - lépe řešený
+
+        Console.Write("Zadejte první číslo: ");
+        int first;
+        while(!int.TryParse(Console.ReadLine(), out first))
+            {
+                Console.Write("Nezadali jste celé číslo, zadejte první číslo znovu: ");
+            }
+
+                
+        Console.ReadKey();
+    }
+}
