222b049afa289f3e8632024231f161c313c8b125
Adam Hettler <ahettler@outlook.cz>
2024-11-06 11:41:53 +0100

    #3 - vykreslení N

---
+++ IS-Projekty/prgram003ca-dalsi-obrazec/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("******** Písmeno N *********");
+            Console.WriteLine("******* Adam Hettler *******");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+
+            Console.Write("Zadejte výšku obrazce (kladné celé číslo): ");
+            uint height;
+            while (!uint.TryParse(Console.ReadLine(), out height) || height == 0)
+            {
+                Console.Write("Nezadali jste kladné celé číslo. Zadejte číslo (celé číslo): ");
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Zadaná výška: {0}", height);
+            Console.WriteLine("========================================================");
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (j == 0 || j == height - 1 || j == i) // vykreslení N
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine();
+        }
+    }
+}
