22edc48053642a453766dead07d031d2d34605ab
Adam Hettler <ahettler@outlook.cz>
2024-11-13 14:49:31 +0100

    #1

--- IS-Projekty/program001-vypis-rady/obj/Debug/net8.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Projekty/program001-vypis-rady/obj/Debug/net8.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c006ba7d0ce48b460a0efa2ddacc5972105f643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006669fcb150e36826b1dd68a5db989fbf6334cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
